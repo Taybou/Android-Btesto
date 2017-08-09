5dff31ae461dc16dba141e9f849cebe72e496678
nour saidi <nourelislam.saidi@canal-plus.com>
2017-08-09 15:34:04 +0200

    add lang and currancy list choices

---
+++ app/src/main/java/com/example/nourelislamsaidi/activities/LangListActivity.java
@@ -0,0 +1,124 @@
+package com.example.nourelislamsaidi.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.example.nourelislamsaidi.adapters.LangAdapter;
+import com.example.nourelislamsaidi.listeners.LanguesRecyclerViewListener;
+import com.example.nourelislamsaidi.numbertowords.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LangListActivity extends AppCompatActivity {
+
+    private static final String LANGUE = "langue";
+    private static final String CURRANCY = "currancy";
+    private static final int LANG_LIST = 1;
+    private static final int CURRANCY_LIST = 2;
+    private static boolean mIsLangue = false;
+    private static boolean mIsCurrancy = false;
+    private RecyclerView mLangRecyclerView;
+    private LangAdapter mAdapter;
+    private List<String> mDataList = new ArrayList<>();
+    private String textChecked;
+    private Activity mActivity;
+
+    @NonNull
+    public static Intent newIntent(Activity activity, String langues, int id) {
+        Intent intent;
+        intent = new Intent(activity, LangListActivity.class);
+        switch (id) {
+            case LANG_LIST:
+                mIsLangue = true;
+                intent.putExtra(LANGUE, langues);
+                break;
+
+            case CURRANCY_LIST:
+                mIsCurrancy = true;
+                intent.putExtra(CURRANCY, langues);
+                break;
+        }
+        return intent;
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        mActivity = this;
+        mLangRecyclerView = (RecyclerView) findViewById(R.id.lang_recycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mLangRecyclerView.setLayoutManager(mLayoutManager);
+        mLangRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        getExtraIntent();
+        mAdapter = new LangAdapter(mDataList, textChecked);
+        mLangRecyclerView.setAdapter(mAdapter);
+
+        mLangRecyclerView.addOnItemTouchListener(new LanguesRecyclerViewListener(getBaseContext(), new LanguesRecyclerViewListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Intent intent;
+                if (mDataList.contains("Euro")) {
+
+                    intent = MainActivity.newMainIntent(mActivity, mDataList.get(position), 2);
+                } else {
+                    intent = MainActivity.newMainIntent(mActivity, mDataList.get(position), 1);
+
+                }
+                startActivity(intent);
+            }
+        }));
+
+        prepareData();
+    }
+
+    private void prepareData() {
+        if (mIsLangue) {
+            mDataList.add("Français");
+            mDataList.add("Arabe");
+            mIsLangue = false;
+            mAdapter.notifyDataSetChanged();
+        }
+
+        if (mIsCurrancy) {
+            mDataList.add("Euro");
+            mDataList.add("Dollar");
+            mDataList.add("Dinar Algérien");
+            mDataList.add("Yen Japonais");
+            mDataList.add("Franc Suisse");
+            mDataList.add("Yuan Renmimbi");
+            mDataList.add("Livre Sterling");
+            mDataList.add("Lira Turque");
+            mDataList.add("Rouble Russe");
+            mDataList.add("Roupie Indienne");
+            mDataList.add("Réal Brésilien");
+            mDataList.add("Rand Sud-africain");
+            mIsCurrancy = false;
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+    private void getExtraIntent() {
+        Intent intent = getIntent();
+        String langueChecked = intent.getStringExtra(LANGUE);
+        String currancyChecked = intent.getStringExtra(CURRANCY);
+
+        if (langueChecked != null) {
+            textChecked = langueChecked;
+        }
+
+        if (currancyChecked != null) {
+            textChecked = currancyChecked;
+        }
+
+    }
+}
--- app/src/main/java/com/example/nourelislamsaidi/activities/MainActivity.java
+++ app/src/main/java/com/example/nourelislamsaidi/activities/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.nourelislamsaidi.activities;
 
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -34,10 +37,16 @@ import com.example.nourelislamsaidi.views.LanguageBtnView;
 import java.util.ArrayList;
 import java.util.Locale;
 
+import static com.example.nourelislamsaidi.activities.LangListActivity.newIntent;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
         View.OnClickListener {
 
+    private static final String LANGUE = "langue";
+    private static final String CURRANCY = "currancy";
+    private static final String SHARED_PREF_KEY = "textKey";
+    private static String mLastLanguage;
     private LanguageBtnView mLanguageBtnView;
     private LanguageBtnView mCurrencyBtnView;
 
@@ -54,6 +63,24 @@ public class MainActivity extends AppCompatActivity
 
     Animation mAnim;
     private ClipboardManager mClipboardManager;
+    private String mLangueText;
+    private String mCurrancyText;
+
+    @NonNull
+    public static Intent newMainIntent(Activity activity, String langues, int id) {
+        Intent intent = new Intent(activity, MainActivity.class);
+        switch (id) {
+            case 1:
+                intent.putExtra(LANGUE, langues);
+                break;
+            case 2:
+                intent.putExtra(CURRANCY, langues);
+                break;
+
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -148,16 +175,43 @@ public class MainActivity extends AppCompatActivity
             case R.id.copy_btn:
                 copyText();
                 break;
-            case R.id.speech_to_txt_btn :
+            case R.id.speech_to_txt_btn:
                 promptSpeechInput();
                 break;
+            case R.id.language_btn:
+                startActivity(this, 1);
+                break;
+            case R.id.currency_btn:
+                startActivity(this, 2);
 
         }
     }
 
+    private void startActivity(Activity activity, int id) {
+        Intent intent = null;
+        switch (id) {
+            case 1:
+                intent = newIntent(activity, mLanguageBtnView.getText(), id);
+                break;
+            case 2:
+                intent = newIntent(activity, mCurrencyBtnView.getText(), id);
+                break;
+        }
+        startActivity(intent);
+    }
+
     private void initViews() {
+        SharedPreferences sp = getSharedPreferences(SHARED_PREF_KEY, 0);
+        String langueText = sp.getString(LANGUE, "");
+        String currancyText = sp.getString(CURRANCY, "");
         mLanguageBtnView = (LanguageBtnView) findViewById(R.id.language_btn);
+        mLanguageBtnView.setText(!currancyText.isEmpty() ? langueText : getResources().getString(R.string.default_lang));
+        mLanguageBtnView.setOnClickListener(this);
+
         mCurrencyBtnView = (LanguageBtnView) findViewById(R.id.currency_btn);
+        mCurrencyBtnView.setText(!currancyText.isEmpty() ? currancyText : getResources().getString(R.string.default_currancy));
+        mCurrencyBtnView.setOnClickListener(this);
+
         mEditNumber = (EditText) findViewById(R.id.edit_number);
         mWordAmount = (TextView) findViewById(R.id.word_amount);
 
@@ -173,18 +227,35 @@ public class MainActivity extends AppCompatActivity
         mSpeechToTextView = (ImageView) findViewById(R.id.speech_to_txt_btn);
         mSpeechToTextView.setOnClickListener(this);
 
-        mLanguageBtnView.setIconVisibility(View.GONE);
-        mCurrencyBtnView.setIconVisibility(View.GONE);
-
         animateEditText(true);
         onEditNumberTextChange();
     }
 
     private void setData() {
-        String language = getResources().getString(R.string.language_text);
+        SharedPreferences sp = getSharedPreferences(SHARED_PREF_KEY, 0);
+        SharedPreferences.Editor sedt = sp.edit();
+        getExtraIntent();
+        if (mLangueText != null) {
+            if (!mLangueText.isEmpty()) {
+                mLanguageBtnView.setText(mLangueText);
+                sedt.putString(LANGUE, mLanguageBtnView.getText().toString());
+            }
+        }
+        if (mCurrancyText != null) {
+            if (!mCurrancyText.isEmpty()) {
+                mCurrencyBtnView.setText(mCurrancyText);
+                sedt.putString(CURRANCY, mCurrencyBtnView.getText().toString());
+            }
+        }
+        sedt.commit();
+    }
 
-        mLanguageBtnView.setText(language);
-        mCurrencyBtnView.setText("EURO");
+    private void getExtraIntent() {
+        Intent intent = getIntent();
+        String langueText = intent.getStringExtra(LANGUE);
+        String currancy = intent.getStringExtra(CURRANCY);
+        mLangueText = langueText;
+        mCurrancyText = currancy;
     }
 
     private void onEditNumberTextChange() {
@@ -197,8 +268,6 @@ public class MainActivity extends AppCompatActivity
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (s.length() != 0) {
                     animateEditText(false);
-
-                    Utils.setContext(getApplicationContext());
                     String frenchWord = Utils.getFrenchWord(s.toString(), mCurrencyBtnView.getText());
                     mWordAmount.setText(frenchWord);
                 } else {
@@ -264,7 +333,7 @@ public class MainActivity extends AppCompatActivity
 
     /**
      * Showing google speech input dialog
-     * */
+     */
     private void promptSpeechInput() {
         mEditNumber.setText("");
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
@@ -284,7 +353,7 @@ public class MainActivity extends AppCompatActivity
 
     /**
      * Receiving speech input
-     * */
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -296,8 +365,8 @@ public class MainActivity extends AppCompatActivity
                     ArrayList<String> result = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     String amount = result.get(0).replaceAll("[^\\d.]", "");
-                    if(!amount.isEmpty()){
-                    mEditNumber.setText(amount);
+                    if (!amount.isEmpty()) {
+                        mEditNumber.setText(amount);
                     } else {
                         Toast.makeText(this, R.string.speech_almount_error, Toast.LENGTH_SHORT).show();
                         mEditNumber.setText("");
